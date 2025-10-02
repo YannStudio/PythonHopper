792ae54e6462349b88bcd067087575071c87a2e7
info <info@YANN_DESKTOP>
2025-10-02 18:07:50 +0200

    SolidworksConnector app added in c#

---
+++ Solidworks connector/SolidworksConnector/SolidworksConnector/SolidworksBridge.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+
+
+namespace SolidworksConnector
+{
+    public static class SolidworksBridge
+    {
+        public static SldWorks Connect(bool visible = true)
+        {
+            SldWorks app;
+            try
+            {
+                app = (SldWorks)Marshal.GetActiveObject("SldWorks.Application");
+            }
+            catch (COMException)
+            {
+                app = new SldWorks();
+            }
+
+            app.Visible = visible;
+            return app;
+        }
+
+    }
+}
